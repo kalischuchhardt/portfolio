43d1bce7fbd416ce23bb0da1c7c2524ae96773cb
kalischuchhardt <148989348+kalischuchhardt@users.noreply.github.com>
2026-09-14 21:47:49 -0400

    Fix deployment merge conflicts

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
 import './index.css'
-import { HeroUIProvider } from '@heroui/react'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <HeroUIProvider>
-      <App />
-    </HeroUIProvider>
-  </React.StrictMode>,
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
